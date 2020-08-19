ee130604720a37976af3dbdc302a384c5c5d3820
CodeWavePro <andrew.stezenko@gmail.com>
2020-08-19 22:57:38 +0300

    Mobile menu fixed, shadow added.

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,32 +10,36 @@ let Navigation = ( props ) => {
         <nav className = { s.navigation }>
             <FontAwesomeIcon icon = { faBars } className = "navigation__icon" />
 
-        	<ul className = { s['navigation-menu'] }>
-        		<li className = { s['navigation-menu__item'] }>
-        			<NavLink className = { s['navigation-menu__link'] }
-                             activeClassName = { s.active }
-                             to = "/cocktails">Cocktails</NavLink>
-                </li>
-                <li className = {s['navigation-menu__item']}>
-        			<NavLink className = {s['navigation-menu__link']}
-                             activeClassName = {s.active}
-                             to = "/ordinary-drinks">Ordinary Drinks</NavLink>
-                </li>
-                <li className = {s['navigation-menu__item']}>
-                    <NavLink className = {s['navigation-menu__link']}
-                             activeClassName = {s.active}
-                             to = "/alcoholic">
-                        Alcoholic
-                    </NavLink>
-                </li>
-                <li className = {s['navigation-menu__item']}>
-                    <NavLink className = {s['navigation-menu__link']}
-                             activeClassName = {s.active}
-                             to = "/non-alcoholic">
-                        Non-Alcoholic
-                    </NavLink>
-                </li>
-        	</ul>
+            <div className = { s['navigation-menu-wrapper'] }>
+                <div className = { s['navigation-menu-inner'] }>
+                    <ul className = { s['navigation-menu'] }>
+                        <li className = { s['navigation-menu__item'] }>
+                            <NavLink className = { s['navigation-menu__link'] }
+                                     activeClassName = { s.active }
+                                     to = "/cocktails">Cocktails</NavLink>
+                        </li>
+                        <li className = {s['navigation-menu__item']}>
+                            <NavLink className = {s['navigation-menu__link']}
+                                     activeClassName = {s.active}
+                                     to = "/ordinary-drinks">Ordinary Drinks</NavLink>
+                        </li>
+                        <li className = {s['navigation-menu__item']}>
+                            <NavLink className = {s['navigation-menu__link']}
+                                     activeClassName = {s.active}
+                                     to = "/alcoholic">
+                                Alcoholic
+                            </NavLink>
+                        </li>
+                        <li className = {s['navigation-menu__item']}>
+                            <NavLink className = {s['navigation-menu__link']}
+                                     activeClassName = {s.active}
+                                     to = "/non-alcoholic">
+                                Non-Alcoholic
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
 
             <SearchContainer />
         </nav>
